517f916ce77ceb69e81f80a6dfeb922cc4f36bd3
Tanish Panjwani <tanish@paywise.io>
2021-07-22 23:59:53 +0300

    added new featueres

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Products, Navbar } from './components'
+import React, { useState, useEffect }from 'react'
+import { Products, Navbar, Cart} from './components'
+import { commerce } from './lib/commerce'
+
 const App = () => {
+    const [ products, setProducts ] = useState([])
+    const [cart, setCart] = useState({})
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list()
+        setProducts(data)
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    useEffect(()=> {
+        fetchProducts()
+        fetchCart()
+    },[])
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+        setCart(item.cart)
+    }
+
     return (
         <div>
-            <Products />
-            <Navbar />
+            {/* <Products products={products} onAddToCart={ handleAddToCart } /> */}
+            <Navbar totalItems={cart.total_items} />
+            <Cart cart={ cart } />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product'
-const products = [
-    { id: 1, name: "Shoes", description: 'Running Shoes.', price: '$5'},
-    { id: 2, name: "Macbook", description: 'Running Macbook.', price: '$5'},
-    { id: 3, name: "Apple", description: 'Running Apple.', price: '$5'},
-]
+import useStyles from './styles'
 
-const Products = () => {
+
+
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} >
-                        <Product product={ product }/>
+                        <Product product={ product } onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
